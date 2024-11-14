892353c99cdfccdd29128232f7b6fc05fb5ae911
valhub08 <jhstl1203@gmail.com>
2024-11-14 23:02:28 +0900

    first commit

---
+++ src/main/java/com/example/areumariserver/AreumariServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.areumariserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreumariServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AreumariServerApplication.class, args);
+    }
+
+}
