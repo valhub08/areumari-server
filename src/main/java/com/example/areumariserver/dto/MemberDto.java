73540977ecd18824deab38ee079269b3ad829b38
valhub08 <jhstl1203@gmail.com>
2024-12-02 20:24:01 +0900

    feat :: MemberDto.java

---
+++ src/main/java/com/example/areumariserver/dto/MemberDto.java
@@ -0,0 +1,38 @@
+package com.example.areumariserver.dto;
+
+import com.example.areumariserver.entity.Member;
+import lombok.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String nickname;
+    private String address;
+    private String phone;
+
+    static public MemberDto toDto(Member member) {
+        return MemberDto.builder()
+                .id(member.getId())
+                .username(member.getUsername())
+                .nickname(member.getNickname())
+                .address(member.getAddress())
+                .phone(member.getPhone())
+                .build();
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .id(id)
+                .username(username)
+                .nickname(nickname)
+                .address(address)
+                .phone(phone)
+                .build();
+    }
+}
