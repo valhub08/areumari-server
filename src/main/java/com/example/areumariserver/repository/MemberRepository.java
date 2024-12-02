3595499fa99319a3213a0a78e31160eb59aa0a0a
valhub08 <jhstl1203@gmail.com>
2024-12-02 20:23:54 +0900

    feat :: MemberRepository.java

---
+++ src/main/java/com/example/areumariserver/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.areumariserver.repository;
+
+import com.example.areumariserver.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+
+    boolean existsByUsername(String username); // 추가
+}
