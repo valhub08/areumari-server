af4579a97ebb4a890ae2e85498e301ac0e8fc3d5
valhub08 <jhstl1203@gmail.com>
2024-12-02 20:22:39 +0900

    feat :: RedisConfig.java

---
+++ src/main/java/com/example/areumariserver/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.areumariserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("localhost");
+        config.setPort(6380); // 6380 포트로 변경
+        config.setPassword("Amgfaa289!@"); // Redis 비밀번호 설정
+        return new LettuceConnectionFactory(config);
+    }
+}
+
+
+
+
