1475926c641962ae15c4ab5b408bb9c91a5d7922
valhub08 <jhstl1203@gmail.com>
2024-12-02 20:24:39 +0900

    feat :: CustomUserDetailsService.java

---
+++ src/main/java/com/example/areumariserver/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.areumariserver.service;
+
+import com.example.areumariserver.entity.Member;
+import com.example.areumariserver.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByUsername(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword()) // 이미 암호화된 비밀번호를 그대로 사용
+                .roles(member.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
+
